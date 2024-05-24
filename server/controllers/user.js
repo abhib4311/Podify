f33f1ebfac79f48067c9390a6bf9e1f127103762
Abhishek Singh <120316690+abhib4311@users.noreply.github.com>
2024-05-24 20:34:25 +0530

    Update user.js for Error Handling

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 import { createError } from "../error.js";
 import User from "../models/User.js";
 
-
+// Update user information
 export const update = async (req, res, next) => {
     if (req.params.id === req.user.id) {
         try {
@@ -10,38 +10,39 @@ export const update = async (req, res, next) => {
                 {
                     $set: req.body,
                 },
-                { new: true }
+                { new: true } // Return the updated document
             );
             res.status(200).json(updatedUser);
         } catch (err) {
-            next(err);
+            next(err); // Pass error to error-handling middleware
         }
     } else {
-        return next(createError(403, "You can update only your account!"));
+        return next(createError(403, "You can update only your account!")); // Unauthorized update attempt
     }
-}
+};
 
+// Get user information along with populated podcasts and favorites
 export const getUser = async (req, res, next) => {
     try {
-        const user = await User.findById(req.user.id).populate({
-            path: "podcasts",
-            populate: {
-                path: "creator",
-                select: "name img",
-            }
-        }
-        ).populate(
-            {
+        const user = await User.findById(req.user.id)
+            .populate({
+                path: "podcasts",
+                populate: {
+                    path: "creator",
+                    select: "name img", // Select only necessary fields
+                },
+            })
+            .populate({
                 path: "favorits",
                 populate: {
                     path: "creator",
-                    select: "name img",
-                }
-            }
-        );
-        res.status(200).json(user);
+                    select: "name img", // Select only necessary fields
+                },
+            });
+        
+        res.status(200).json(user); // Send user data as JSON
     } catch (err) {
-        console.log(req.user)
-        next(err);
+        console.error(err); // Log error to console
+        next(err); // Pass error to error-handling middleware
     }
-}
+};
